cbdef29548f835af755cbc2141d10acb0b00cb78
Hector Siman <hector@verticis.com.br>
2022-01-25 13:46:44 -0300

    update(): Customization of centered slides

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(".project-detail").slick({
     slidesToShow: 1,
     arrows: false,
     asNavFor: ".project-strip",
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000
 });
 
@@ -16,3 +16,38 @@ $(".project-strip").slick({
     centerMode: true,
     focusOnSelect: true
 });
+
+
+
+function getCenteredSlideHeight() {
+    var slides = document.querySelectorAll('.project-strip .slick-slide:not(.slick-current)')
+    var centeredSlide = document.querySelector('.project-strip .slick-current')
+    var featuredSlide = document.querySelector('.project-detail .slick-current')
+    centeredSlide.style.transition = '0.3s all ease-in-out'
+    centeredSlide.style.transform = 'scale(1.2)'
+    centeredSlide.style.zIndex = 99
+    centeredSlide.style.filter = 'none'
+    document.querySelector('.project-strip .draggable').style.height = 40 + centeredSlide.offsetHeight + 'px'
+    document.querySelector('.project-strip .slick-track').style.top = 20 + 'px'
+    return
+}
+function setAllImagesScaleDown() {
+    var slides = document.querySelectorAll('.project-strip .slick-slide:not(.slick-current)')
+    slides.forEach(element => {
+        element.style.transform = 'scale(1)'
+        element.style.zIndex = -1
+        element.style.position = 'relative'
+        element.style.filter = 'blur(4px)'
+
+    });
+}
+
+$('.project-strip .slick-slide, .project-detail .slick-current').click(function () {
+    getCenteredSlideHeight()
+    console.log('clicked');
+    setAllImagesScaleDown()
+}).on(function () {
+    getCenteredSlideHeight()
+    console.log('chaged');
+    setAllImagesScaleDown()
+})
